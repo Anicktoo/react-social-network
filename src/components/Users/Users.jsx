0af177f3e22f8375f411dc40814d1a486cee2a0f
anicktoo <anickt00@gmail.com>
2024-02-27 01:12:56 +0300

    56. User container and presentation components

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,141 +1,95 @@
-import React from 'react';
-import s from './Users.module.css';
-import {classNameFunction} from "../../tools/classNameCompiler";
+import s from "./Users.module.css";
 import UserItemContainer from "./Usertem/UserItemContainer";
-import axios from "axios";
+import {classNameFunction} from "../../tools/classNameCompiler";
 
 const cName = classNameFunction(s);
 
+const Users = (props) => {
 
-// const Users = (props) => {
-//
-//     const getUsers = () => {
-//         if (props.users.length === 0) {
-//
-//             axios.get('https://social-network.samuraijs.com/api/1.0/users')
-//                 .then(response => props.setUsers(response.data.items))
-//
-//         }
-//     }
-//     const usersList = props.users.map(el => <UserItemContainer key={el.id} id={el.id}/>);
-//
-//     return (
-//         <main className={s.friends}>
-//             <button onClick={getUsers}>Get Users</button>
-//             <div className={cName(['friendsContainer', 'stdBlock'])}>
-//                 <header className={s.header}>
-//                     <span>All Users</span>
-//                 </header>
-//                 <div className={s.usersList}>
-//                     {usersList}
-//                 </div>
-//             </div>
-//         </main>
-//     );
-// };
-//
-// export default Users;
+    const MAX_USERS_ON_PAGE = 11;
 
-class Users extends React.Component {
-    componentDidMount() {
-        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`)
-            .then(response => {
-                this.props.setUsers(response.data.items);
-                this.props.setTotalUsersCount(response.data.totalCount);
-            });
-    }
-
-    pageChange(pageNumber) {
-        this.props.setCurrentPage(pageNumber);
-        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`)
-            .then(response => this.props.setUsers(response.data.items));
-    }
-
-    render() {
-        const getStartEndIndexes = (maxNumbersOnPage, curPage, pagesCount) => {
-            if (pagesCount <= maxNumbersOnPage) {
-                return {startInd: 1, endInd: pagesCount};
-            }
-
-            const delta = Math.floor(maxNumbersOnPage / 2);
-            const pagesToFirst = curPage - 1;
-            const pagesToLast = pagesCount - curPage;
-
-            if (pagesToFirst <= delta) {
-                return {startInd: 1, endInd: maxNumbersOnPage};
-            }
-            if (pagesToLast <= delta) {
-                return {startInd: pagesCount - maxNumbersOnPage, endInd: pagesCount};
-            }
-
-            return {startInd: curPage - delta, endInd: curPage + delta};
+    const getStartEndIndexes = (maxNumbersOnPage, curPage, pagesCount) => {
+        if (pagesCount <= maxNumbersOnPage) {
+            return {startInd: 1, endInd: pagesCount};
         }
 
-        const maxNumbersOnPage = 11;
-        const curPage = this.props.currentPage;
-        const pagesCount = Math.ceil(this.props.totalUsersCount / this.props.pageSize);
+        const delta = Math.floor(maxNumbersOnPage / 2);
+        const pagesToFirst = curPage - 1;
+        const pagesToLast = pagesCount - curPage;
 
-        let {startInd, endInd} = getStartEndIndexes(maxNumbersOnPage, curPage, pagesCount);
-
-        const startGap = startInd !== 1;
-        const endGap = endInd !== pagesCount;
-        
-        if (startGap) startInd += 2;
-        if (endGap) endInd -= 2;
-
-        const pages = [];
-        for (let i = startInd; i <= endInd; i++) {
-            pages.push(i);
+        if (pagesToFirst <= delta) {
+            return {startInd: 1, endInd: maxNumbersOnPage};
         }
-        return (
-            <main className={s.friends}>
-                <div className={cName(['friendsContainer', 'stdBlock'])}>
-                    <header className={s.header}>
-                        <span>All Users</span>
-                        <div className={cName(['page-container'])}>
-                            {startGap ?
-                                <>
-                                    <div className={cName(['page-item'])}
+        if (pagesToLast <= delta) {
+            return {startInd: pagesCount - maxNumbersOnPage, endInd: pagesCount};
+        }
+
+        return {startInd: curPage - delta, endInd: curPage + delta};
+    }
+
+    const curPage = props.currentPage;
+    const pagesCount = Math.ceil(props.totalUsersCount / props.pageSize);
+
+    let {startInd, endInd} = getStartEndIndexes(MAX_USERS_ON_PAGE, curPage, pagesCount);
+
+    const startGap = startInd !== 1;
+    const endGap = endInd !== pagesCount;
+
+    if (startGap) startInd += 2;
+    if (endGap) endInd -= 2;
+
+    const pages = [];
+    for (let i = startInd; i <= endInd; i++) {
+        pages.push(i);
+    }
+    return (
+        <main className={s.friends}>
+            <div className={cName(['friendsContainer', 'stdBlock'])}>
+                <header className={s.header}>
+                    <span>All Users</span>
+                    <div className={cName(['page-container'])}>
+                        {startGap ?
+                            <>
+                                <div className={cName(['page-item'])}
+                                     onClick={() => {
+                                         props.pageChange(1)
+                                     }}
+                                >
+                                    1
+                                </div>
+                                <div className={cName(['page-item', 'blocked'])}>...</div>
+                            </> : ''}
+                        {pages.map(el => {
+                                const isCurrent = curPage === el ? 'current-page' : '';
+                                return (
+                                    <div className={cName(['page-item', isCurrent])}
                                          onClick={() => {
-                                             this.pageChange(1)
+                                             props.pageChange(el)
                                          }}
                                     >
-                                        1
-                                    </div>
-                                    <div className={cName(['page-item', 'blocked'])}>...</div>
-                                </> : ''}
-                            {pages.map(el => {
-                                    const isCurrent = curPage === el ? 'current-page' : '';
-                                    return (
-                                        <div className={cName(['page-item', isCurrent])}
-                                             onClick={() => {
-                                                 this.pageChange(el)
-                                             }}
-                                        >
-                                            {el}
-                                        </div>);
-                                }
-                            )}
-                            {endGap ?
-                                <>
-                                    <div className={cName(['page-item', 'blocked'])}>...</div>
-                                    <div className={cName(['page-item'])}
-                                         onClick={() => {
-                                             this.pageChange(pagesCount)
-                                         }}
-                                    >
-                                        {pagesCount}
-                                    </div>
-                                </> : ''}
-                        </div>
-                    </header>
-                    <div className={s.usersList}>
-                        {this.props.users.map(el => <UserItemContainer key={el.id} userInfo={el}/>)}
+                                        {el}
+                                    </div>);
+                            }
+                        )}
+                        {endGap ?
+                            <>
+                                <div className={cName(['page-item', 'blocked'])}>...</div>
+                                <div className={cName(['page-item'])}
+                                     onClick={() => {
+                                         props.pageChange(pagesCount)
+                                     }}
+                                >
+                                    {pagesCount}
+                                </div>
+                            </> : ''}
                     </div>
+                </header>
+                <div className={s.usersList}>
+                    {props.users.map(el => <UserItemContainer key={el.id} userInfo={el}/>)}
                 </div>
-            </main>
-        );
-    };
-}
+            </div>
+        </main>
+    );
+};
 
 export default Users;
--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -1,6 +1,32 @@
 import {connect} from 'react-redux';
-import Users from './Users';
 import {setCurrentPageAC, setTotalUsersCountAC, setUsersAC} from "../../redux/usersReducer";
+import React from "react";
+import axios from "axios";
+import Users from "./Users";
+
+class UsersContainer extends React.Component {
+    componentDidMount() {
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${this.props.currentPage}&count=${this.props.pageSize}`)
+            .then(response => {
+                this.props.setUsers(response.data.items);
+                this.props.setTotalUsersCount(response.data.totalCount);
+            });
+    }
+
+    pageChange(pageNumber) {
+        this.props.setCurrentPage(pageNumber);
+        axios.get(`https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.pageSize}`)
+            .then(response => this.props.setUsers(response.data.items));
+    }
+
+    render() {
+        return <Users totalUsersCount={this.props.totalUsersCount}
+                      currentPage={this.props.currentPage}
+                      pageSize={this.props.pageSize}
+                      users={this.props.users}
+                      pageChange={this.pageChange.bind(this)}/>
+    };
+}
 
 const mapStateToProps = (state, props) => {
     return {
@@ -19,6 +45,4 @@ const mapDispatchToProps = (dispatch) => {
     };
 }
 
-const UsersContainer = connect(mapStateToProps, mapDispatchToProps)(Users);
-
-export default UsersContainer;
+export default connect(mapStateToProps, mapDispatchToProps)(UsersContainer);
